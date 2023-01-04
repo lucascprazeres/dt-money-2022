53e1512caa772e9695de56e3c799bb4eaed4e733
Lucas Prazeres <lucascprazeres@outlook.com>
2023-01-04 18:59:50 -0300

    feat: formata valores monetarios e de data

---
+++ src/components/Summary/methods.ts
@@ -0,0 +1,22 @@
+import { Transaction } from '../../contexts/TransactionsContext'
+
+export function calculateSummary(transactions: Transaction[]) {
+  return transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+}
